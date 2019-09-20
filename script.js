7c0a32c2f0447e01f25c401b747ada3c4ecaeb0b
jEF <alexxander.00@outlook.com>
2019-09-20 13:50:54 +0200

    source

--- script.js
+++ script.js
@@ -2,27 +2,65 @@ const btnLow = document.getElementById("Low");
 const btnCorrect = document.getElementById("Correct");
 const btnHigh = document.getElementById("High");
 
-btnLow.addEventListener("click", GuessNumber("low"));
+let lastGuess = 500;
+let answer = Math.ceil(lastGuess);
 
-btnHigh.addEventListener("click", GuessNumber("high"));
+btnLow.addEventListener("click", function () {
+    GuessNumber("low");
+});
+
+btnHigh.addEventListener("click", function () {
+    GuessNumber("high");
+});
 
 btnCorrect.addEventListener("click", GuessNumber("correct"));
 
 
+var X = 1;
+var Y = 1001;
+
+var newGuess;
 
 
-let lastGuess = 500;
-
-var rd = Math.floor(Math.random() * lastGuess);
+//var rd = Math.floor(Math.random() * lastGuess);
 
 function GuessNumber(input) {
+    
     if (input == "correct") {
-
+        console.log("yesyesysy");
     }
-    else if (input == "low") {
+    else if (input == "high" && lastGuess >= 500) {
+        X = Y - lastGuess;
+        newGuess = Math.floor(X / 2);
+        lastGuess = lastGuess + newGuess;
 
+        console.log(answer);
     }
-    else if (input == "high") {
 
+
+    else if (input == "low" && lastGuess > 500) {
+        newGuess = Math.floor((lastGuess - 500) / 2);
+        lastGuess = lastGuess - newGuess;
+        
+
+        console.log(answer);
     }
+
+
+
+    else if (input == "high" && lastGuess < 500) {
+        lastGuess= Math.floor((lastGuess / 2) + lastGuess);
+
+
+        console.log(lastGuess);
+    }
+
+
+    else if (input == "low" && lastGuess <= 500) {
+        lastGuess = Math.floor(lastGuess / 2);
+
+        console.log(lastGuess);
+    }
+    
+
 }
