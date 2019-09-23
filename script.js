5e5d0184db16265798f995df97d5a7721f801a5f
jEF <alexxander.00@outlook.com>
2019-09-23 10:57:20 +0200

    counter

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ const btnHigh = document.getElementById("High");
 const showBox = document.getElementById("showNr");
 
 
-btnLow.addEventListener("click", function() {
+btnLow.addEventListener("click", function () {
     GuessNumber("low");
 });
 
-btnHigh.addEventListener("click", function() {
+btnHigh.addEventListener("click", function () {
     GuessNumber("high");
 });
 
-btnCorrect.addEventListener("click", function() {
+btnCorrect.addEventListener("click", function () {
     GuessNumber("correct")
 });
 
@@ -21,29 +21,30 @@ let i = 0;
 let lastGuess = 500;
 var newGuess = 250;
 showBox.innerHTML = "the computers guess is " + lastGuess;
-//var rd = Math.floor(Math.random() * lastGuess);
+
 
 function GuessNumber(input) {
 
-    if (input == "low" && i <= 10) {
+    if (i == 10) {
+        showBox.innerHTML = "Max guesses reached";
+    }
+    else if (input == "low") {
         lastGuess = Math.round(lastGuess -= newGuess);
         Math.round(newGuess /= 2);
         showBox.innerHTML = "the computers guess is " + Math.round(lastGuess);
         console.log(lastGuess);
         i++;
-    } else if (input == "high" && i <= 10) {
+    } else if (input == "high") {
         lastGuess = Math.round(lastGuess += newGuess);
         Math.round(newGuess /= 2);
         showBox.innerHTML = "the computers guess is " + Math.round(lastGuess);
         console.log(lastGuess);
         i++;
-    } else if (input == "correct" && i <= 10) {
+    } else if (input == "correct") {
         showBox.innerHTML = "the computers guess is correct";
         lastGuess = 500;
         newGuess = 250;
         i = 0;
-    } else {
-        showBox.innerHTML = "You are dont have a number";
     }
 
 
